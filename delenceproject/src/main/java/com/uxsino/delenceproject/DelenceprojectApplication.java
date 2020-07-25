ec2fd7a919eabd296bda56f05cc43b474df40022
970019ypf <2238013231@qq.com>
2020-07-25 18:32:11 +0800

    创建springboot骨架

---
+++ delenceproject/src/main/java/com/uxsino/delenceproject/DelenceprojectApplication.java
@@ -0,0 +1,13 @@
+package com.uxsino.delenceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelenceprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelenceprojectApplication.class, args);
+    }
+
+}
---
+++ delenceproject/src/test/java/com/uxsino/delenceproject/DelenceprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uxsino.delenceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelenceprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
